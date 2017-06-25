c244206f9976b36dca93e6dbb4349adee40c0e36
Gal Lerner <galliz1051@gmail.com>
2017-06-25 23:44:24 +0300

    update

--- client_prototype2/src/controllers/DeletePupilFromCourseController.java
+++ client_prototype2/src/controllers/DeletePupilFromCourseController.java
@@ -1,14 +1,23 @@
 package controllers;
 
+import java.io.IOException;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.ResourceBundle;
 
+import application.Main;
 import interfaces.IController;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.control.Alert.AlertType;
+import javafx.stage.Stage;
+import ui.UserWindow;
 
 public class DeletePupilFromCourseController implements IController
 {
@@ -54,35 +63,137 @@ public class DeletePupilFromCourseController implements IController
 
 	@FXML
 	private Label CourseIDLable;
+	
+	private String PupilID;
+	private String CourseID;
+	private int checkCourseID;
+	private int checkPupilID;
+	private int ExeReFLAG;
 
 	@FXML
 	void CheckPupilID(ActionEvent event)
 	{
-
+		ArrayList<String> data = new ArrayList<String>();
+		data.add("Check Pupil");
+		data.add("select");
+		data.add("pupil");
+		data.add("userID");
+		PupilID=PupilIDTextField.getText();
+		data.add(PupilID);
+		try
+		{
+			Main.client.sendToServer(data);
+		}
+		catch (IOException e)
+		{
+			e.printStackTrace();
+		}
 	}
 
 	@FXML
 	void CheckCourseID(ActionEvent event)
 	{
+		ArrayList<String> data = new ArrayList<String>();
+		data.add("Check Course");
+		data.add("select");
+		data.add("courses");
+		data.add("courseId");
+		CourseID=CourseIDTextField.getText();
+		data.add(CourseID);
 
+		try
+		{
+			Main.client.sendToServer(data);
+		}
+		catch (IOException e)
+		{
+			e.printStackTrace();
+		}
 	}
 
 	@FXML
 	void DeletePupilCourse(ActionEvent event)
 	{
-
+    	if(checkCourseID==1 && checkPupilID==1 && ExeReFLAG==1)
+    	{
+    		loadDecision();
+    	}
 	}
+	
+	void loadDecision()
+	 {
+		ArrayList<String> data = new ArrayList<String>();
+   		data.add("Check Exeptional Request Descision");
+   		data.add("select field");
+   		data.add("descision");
+   		data.add("exceptional_request");
+   		data.add("CourseID");
+   		data.add(CourseIDTextField.getText());
+   		data.add("userID");
+   		data.add(PupilIDTextField.getText());
+   		data.add("type");
+   		data.add("delete");
+   		data.add("exceptonalRequestID");
+   		data.add(RequestIdTextField.getText());
+   		
+			
+		try
+		{
+			Main.client.sendToServer(data);
+		}
+		catch (IOException e)
+		{
+			e.printStackTrace();
+		}  
+	 }
 
 	@FXML
 	void CheckRequestID(ActionEvent event)
 	{
+		ArrayList<String> data = new ArrayList<String>();
+		data.add("Check RequestID");
+		data.add("select");
+		data.add("exceptional_request");
+		data.add("exceptonalRequestID");
+		data.add(RequestIdTextField.getText());
+		data.add("type");
+		data.add("delete");
 
+		try
+		{
+			Main.client.sendToServer(data);
+		}
+		catch (IOException e)
+		{
+			e.printStackTrace();
+		}
 	}
 
 	@FXML
 	void BackToMenu(ActionEvent event)
 	{
+		UserWindow.closeUserWindow(getClass(), (Stage) DeleteButton.getScene().getWindow());
+	}
+	
+	void DeletePupilFromCourse()
+	{
+		ArrayList<String> data = new ArrayList<String>();
+		data.add("Delete Pupil From Course");
+		data.add("delete");
+		data.add("pupil_in_course");
+		data.add("userID");
+		data.add(PupilIDTextField.getText());
+		data.add("courseID");
+		data.add("CourseIDTextField");
 
+		try
+		{
+			Main.client.sendToServer(data);
+		}
+		catch (IOException e)
+		{
+			e.printStackTrace();
+		}
 	}
 
 	@FXML
@@ -101,12 +212,119 @@ public class DeletePupilFromCourseController implements IController
 		assert DeleteLable != null : "fx:id=\"DeleteLable\" was not injected: check your FXML file 'SecretaryDeletePupilFromCourse.fxml'.";
 		assert CourseIDLable != null : "fx:id=\"CourseIDLable\" was not injected: check your FXML file 'SecretaryDeletePupilFromCourse.fxml'.";
 
+		PupilID="";
+		CourseID="";
+	    checkCourseID=0;
+	    checkPupilID=0;
+	    ExeReFLAG=0;
 	}
 
 	@Override
-	public void handleAnswer(Object msg)
+	public void handleAnswer(Object result)
 	{
 		// TODO Auto-generated method stub
+		
+		
+		if (result == null)
+		{
+			// error
+			new Alert(AlertType.ERROR, "Item has not found.", ButtonType.OK).showAndWait();
+			return;
+		}
 
+		ArrayList<String> arr = (ArrayList<String>) result;
+		String type = arr.remove(0);
+			
+		if(type.equals("Check RequestID"))
+		{
+			if(arr.size()==0)
+			{
+				new Alert(AlertType.ERROR, "Exceptional Request has not found.", ButtonType.OK).showAndWait();
+	
+			}
+			else
+			{
+				ExeReFLAG=1;
+				new Alert(AlertType.INFORMATION, "Exceptional Request has found.", ButtonType.OK).showAndWait();
+			}
+		}
+		
+		if (type.equals("Check Pupil"))
+		{
+			if(arr.size() == 0)
+			{
+				new Alert(AlertType.ERROR, "Pupil has not found.", ButtonType.OK).showAndWait();
+			}
+			else
+			{
+				 
+			    checkPupilID=1;
+				new Alert(AlertType.INFORMATION, "Pupil has found.", ButtonType.OK).showAndWait();
+			}
+		}
+		
+		if(type.equals( "Check Course"))
+		{
+			if(arr.size()==0)
+			{
+				new Alert(AlertType.ERROR, "Course has not found.", ButtonType.OK).showAndWait();
+			}
+				
+			else 
+			{
+				checkCourseID=1;
+				new Alert(AlertType.INFORMATION, "Course has found.", ButtonType.OK).showAndWait();
+			}		
+		}
+		
+		if(type.equals("Check Exeptional Request Descision"))
+			
+		{
+		String des="";
+		if(arr.isEmpty()==true)
+		{
+			new Alert(AlertType.ERROR, "Exceptional Request for assigning pupil to course by details you entered not exist.", ButtonType.OK).showAndWait();	
+		}
+		else
+		{
+			for (String row : arr)
+			{
+				String[] cols = row.split(";");
+				HashMap<String, String> map = new HashMap<>();
+				for (String col : cols)
+				{
+					String[] field = col.split("=");
+					map.put(field[0], field[1]);
+				}
+			   des=map.get("descision");
+			}
+			if(des.equals("diny"))
+			{
+				new Alert(AlertType.ERROR, "The requst to assign pupil to this course not confirmed", ButtonType.OK).showAndWait();
+			}
+			else if(des.equals("confirm"))
+			{
+				DeletePupilFromCourse();
+			}
+			else if(des.equals("pending"))
+			{
+				new Alert(AlertType.ERROR, "No Response To This Request Yet", ButtonType.OK).showAndWait();
+			}
+		}
+		}
+		
+		if(type.equals("Delete Pupil From Course"))
+		{
+			if(arr.size()==0)
+				
+			{
+				new Alert(AlertType.ERROR, "Delete failed", ButtonType.OK).showAndWait();
+			}
+			else
+			{
+				new Alert(AlertType.INFORMATION, "Pupil Succesfully Delited From Course", ButtonType.OK).showAndWait();
+				UserWindow.closeUserWindow(getClass(), (Stage) DeleteButton.getScene().getWindow());	
+			}
+		}
 	}
 }
--- client_prototype2/src/controllers/TeacherExeptionalRequestController.java
+++ client_prototype2/src/controllers/TeacherExeptionalRequestController.java
@@ -1,137 +1,322 @@
 package controllers;
 
+import java.io.IOException;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.ResourceBundle;
 
+import application.Main;
 import interfaces.IController;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
-import javafx.scene.control.MenuButton;
-import javafx.scene.control.MenuItem;
 import javafx.scene.control.TextField;
+import javafx.scene.control.Alert.AlertType;
 
-public class TeacherExeptionalRequestController implements IController
-{
+public class TeacherExeptionalRequestController implements IController {
 
-	@FXML
-	private ResourceBundle resources;
+    @FXML
+    private ResourceBundle resources;
 
-	@FXML
-	private URL location;
+    @FXML
+    private URL location;
 
-	@FXML
-	private ComboBox<?> ListTeacherComboBox;
+    @FXML
+    private ComboBox<String> ListTeacherComboBox;
 
-	@FXML
-	private TextField RequestIdTextField;
+    @FXML
+    private TextField RequestIdTextField;
 
-	@FXML
-	private Label OperationLabel;
+    @FXML
+    private Label ClassLabel;
 
-	@FXML
-	private Button CheckIdButton;
+    @FXML
+    private Label TeacherExeptionalLabel;
 
-	@FXML
-	private Button BackButton;
+    @FXML
+    private Label TeacherLabel;
 
-	@FXML
-	private ComboBox<?> ListCurseComboBox;
+    @FXML
+    private Button CheckIdButton;
 
-	@FXML
-	private Label TeacherExeptionalLabel;
+    @FXML
+    private Label RequestIdLabel;
 
-	@FXML
-	private Label TeacherLabel;
+    @FXML
+    private Button BackButton;
 
-	@FXML
-	private MenuItem ReAssignLabel;
+    @FXML
+    private Label CourseLabel1;
 
-	@FXML
-	private Label RequestIdLabel;
+    @FXML
+    private Button SendButton;
 
-	@FXML
-	private Label CourseLabel;
+    @FXML
+    private ComboBox<String> ListCurseComboBox;
 
-	@FXML
-	private MenuButton OperationMenuButton;
+    @FXML
+    private ComboBox<String> ListCLassComboBox;
+    
+    private int ExFlag;
+    private String cbClass;
+    private String ClassID;
+    private String courseid;
+    private String StudyUnit;
+	private String CourseWeeklyHour;
 
-	@FXML
-	private Button SendButton;
+    @FXML
+    void ChooseClass(ActionEvent event) {
+    	
+    	cbClass=ListCLassComboBox.getSelectionModel().getSelectedItem();
+    	String[] Temp=cbClass.split(":");
+    	ClassID=Temp[0];
+    	loadCoursesInClass(ClassID);
+    }
 
-	@FXML
-	void ChooseCourse(ActionEvent event)
+    @FXML
+    void ChooseTeacher(ActionEvent event) {
+
+    }
+
+    @FXML
+    void SendToSchoolManager(ActionEvent event) {
+
+    }
+
+    @FXML
+    void BackToMenu(ActionEvent event) {
+
+    }
+
+    @FXML
+    void CeckIdAvailability(ActionEvent event) {
+
+		ArrayList<String> data = new ArrayList<String>();
+		data.add("Check Availbility Request ID");
+		data.add("select");
+		data.add("exceptional_request");
+		data.add("exceptonalRequestID");
+		data.add(RequestIdTextField.getText());
+
+		try
+		{
+			Main.client.sendToServer(data);
+		}
+		catch (IOException e)
+		{
+			e.printStackTrace();
+		}
+    }
+
+    @FXML
+    void ChooseCourse(ActionEvent event) {
+
+    }
+    
+    void loadClassIDAndName()
+    {
+		ArrayList<String> data = new ArrayList<String>();
+		data.add("Load Class ID and Name");
+		data.add("select");
+		data.add("class");
+		
+		try
+		{
+			Main.client.sendToServer(data);
+		}
+		catch (IOException e)
+		{
+			e.printStackTrace();
+		}
+    }
+    
+    void loadCoursesInClass(String ClassiD)
+    {
+    	ArrayList<String> data = new ArrayList<String>();
+		data.add("Load Course in class");
+		data.add("select");
+		data.add("course_in_class");
+		data.add("classId");
+		data.add(ClassiD);
+		
+		try
+		{
+			Main.client.sendToServer(data);
+		}
+		catch (IOException e)
+		{
+			e.printStackTrace();
+		}
+    }
+    
+	void loadStudyUnit()
 	{
+		ArrayList<String> data = new ArrayList<String>();
+		data.add("Load Study Unit");
+		data.add("select");
+		data.add("courses");
+		data.add("courseId");
+		data.add(courseid);
 
+		try
+		{
+			Main.client.sendToServer(data);
+		}
+		catch (IOException e)
+		{
+			e.printStackTrace();
+		}
 	}
+	
+	 void loadteacher()
+	 {
+			ArrayList<String> data = new ArrayList<String>();
+			data.add("Teacher List");
+			data.add("select");
+			data.add("teacher_in_studyunit");
+			data.add("studyUnitID");
+			data.add(StudyUnit);
 
-	@FXML
-	void ChooseTeacher(ActionEvent event)
-	{
+			try
+			{
+				Main.client.sendToServer(data);
+			}
+			catch (IOException e)
+			{
+				e.printStackTrace();
+			}
+	 }
 
-	}
 
-	@FXML
-	void ChooseOperation(ActionEvent event)
-	{
+    @FXML
+    void initialize() {
+        assert ListTeacherComboBox != null : "fx:id=\"ListTeacherComboBox\" was not injected: check your FXML file 'ExeptionalRequestForTeacher.fxml'.";
+        assert RequestIdTextField != null : "fx:id=\"RequestIdTextField\" was not injected: check your FXML file 'ExeptionalRequestForTeacher.fxml'.";
+        assert ClassLabel != null : "fx:id=\"ClassLabel\" was not injected: check your FXML file 'ExeptionalRequestForTeacher.fxml'.";
+        assert TeacherExeptionalLabel != null : "fx:id=\"TeacherExeptionalLabel\" was not injected: check your FXML file 'ExeptionalRequestForTeacher.fxml'.";
+        assert TeacherLabel != null : "fx:id=\"TeacherLabel\" was not injected: check your FXML file 'ExeptionalRequestForTeacher.fxml'.";
+        assert CheckIdButton != null : "fx:id=\"CheckIdButton\" was not injected: check your FXML file 'ExeptionalRequestForTeacher.fxml'.";
+        assert RequestIdLabel != null : "fx:id=\"RequestIdLabel\" was not injected: check your FXML file 'ExeptionalRequestForTeacher.fxml'.";
+        assert BackButton != null : "fx:id=\"BackButton\" was not injected: check your FXML file 'ExeptionalRequestForTeacher.fxml'.";
+        assert CourseLabel1 != null : "fx:id=\"CourseLabel1\" was not injected: check your FXML file 'ExeptionalRequestForTeacher.fxml'.";
+        assert SendButton != null : "fx:id=\"SendButton\" was not injected: check your FXML file 'ExeptionalRequestForTeacher.fxml'.";
+        assert ListCurseComboBox != null : "fx:id=\"ListCurseComboBox\" was not injected: check your FXML file 'ExeptionalRequestForTeacher.fxml'.";
+        assert ListCLassComboBox != null : "fx:id=\"ListCLassComboBox\" was not injected: check your FXML file 'ExeptionalRequestForTeacher.fxml'.";
 
-	}
-
-	@FXML
-	void ReAssignTeacher(ActionEvent event)
-	{
-
-	}
-
-	@FXML
-	void ReAssignMenu(ActionEvent event)
-	{
-
-	}
-
-	@FXML
-	void SendToSchoolManager(ActionEvent event)
-	{
-
-	}
-
-	@FXML
-	void BackToMenu(ActionEvent event)
-	{
-
-	}
-
-	@FXML
-	void CeckIdAvailability(ActionEvent event)
-	{
-
-	}
-
-	@FXML
-	void initialize()
-	{
-		assert ListTeacherComboBox != null : "fx:id=\"ListTeacherComboBox\" was not injected: check your FXML file 'ExeptionalRequestForTeacher.fxml'.";
-		assert RequestIdTextField != null : "fx:id=\"RequestIdTextField\" was not injected: check your FXML file 'ExeptionalRequestForTeacher.fxml'.";
-		assert OperationLabel != null : "fx:id=\"OperationLabel\" was not injected: check your FXML file 'ExeptionalRequestForTeacher.fxml'.";
-		assert CheckIdButton != null : "fx:id=\"CheckIdButton\" was not injected: check your FXML file 'ExeptionalRequestForTeacher.fxml'.";
-		assert BackButton != null : "fx:id=\"BackButton\" was not injected: check your FXML file 'ExeptionalRequestForTeacher.fxml'.";
-		assert ListCurseComboBox != null : "fx:id=\"ListCurseComboBox\" was not injected: check your FXML file 'ExeptionalRequestForTeacher.fxml'.";
-		assert TeacherExeptionalLabel != null : "fx:id=\"TeacherExeptionalLabel\" was not injected: check your FXML file 'ExeptionalRequestForTeacher.fxml'.";
-		assert TeacherLabel != null : "fx:id=\"TeacherLabel\" was not injected: check your FXML file 'ExeptionalRequestForTeacher.fxml'.";
-		assert ReAssignLabel != null : "fx:id=\"ReAssignLabel\" was not injected: check your FXML file 'ExeptionalRequestForTeacher.fxml'.";
-		assert RequestIdLabel != null : "fx:id=\"RequestIdLabel\" was not injected: check your FXML file 'ExeptionalRequestForTeacher.fxml'.";
-		assert CourseLabel != null : "fx:id=\"CourseLabel\" was not injected: check your FXML file 'ExeptionalRequestForTeacher.fxml'.";
-		assert OperationMenuButton != null : "fx:id=\"OperationMenuButton\" was not injected: check your FXML file 'ExeptionalRequestForTeacher.fxml'.";
-		assert SendButton != null : "fx:id=\"SendButton\" was not injected: check your FXML file 'ExeptionalRequestForTeacher.fxml'.";
-
-	}
+        Main.client.controller=this;
+        ExFlag=0;
+        cbClass="";
+        ClassID="";
+        courseid="";
+        StudyUnit="";
+    	CourseWeeklyHour="";
+        loadClassIDAndName();
+    }
 
 	@Override
-	public void handleAnswer(Object msg)
-	{
+	public void handleAnswer(Object result) {
 		// TODO Auto-generated method stub
+		
+		if (result == null)
+		{
+			// error
+			new Alert(AlertType.ERROR, "Item has not found.", ButtonType.OK).showAndWait();
+			return;
+		}
 
+		ArrayList<String> arr = (ArrayList<String>) result;
+		String type = arr.remove(0);
+		
+		if(type.equals("Check Availbility Request ID"))
+		{
+			if(arr.size()==0)
+			{
+				ExFlag=1;
+				new Alert(AlertType.INFORMATION, "Exceptional Request ID Availible.", ButtonType.OK).showAndWait();
+			}
+			else
+			{
+				new Alert(AlertType.ERROR, "Exceptional Request ID Already Exist.", ButtonType.OK).showAndWait();
+			}
+		}
+		
+		if(type.equals("Load Class ID and Name"))
+		{
+				for (String row : arr)
+				{
+					String[] cols = row.split(";");
+					HashMap<String, String> map = new HashMap<>();
+					for (String col : cols)
+					{
+						String[] field = col.split("=");
+						map.put(field[0], field[1]);
+					}
+
+					String classname = map.get("className");
+					String classID= map.get("classId");
+					ListCLassComboBox.getItems().add(classID + ": " + classname);
+				}
+		}
+		
+		if(type.equals("Load Course in class"))
+		{
+			for (String row : arr)
+			{
+				String[] cols = row.split(";");
+				HashMap<String, String> map = new HashMap<>();
+				for (String col : cols)
+				{
+					String[] field = col.split("=");
+					map.put(field[0], field[1]);
+				}
+
+				courseid= map.get("courseId");
+				ListCurseComboBox.getItems().add(courseid);
+			}
+			loadStudyUnit();
+		}
+		
+		if(type.equals("Load Study Unit"))
+		{
+			for (String row : arr)
+			{
+				String[] cols = row.split(";");
+				HashMap<String, String> map = new HashMap<>();
+				for (String col : cols)
+				{
+					String[] field = col.split("=");
+					map.put(field[0], field[1]);
+				}
+
+				StudyUnit = map.get("studyUnit");
+				CourseWeeklyHour = map.get("weeklyStudyHours");
+			}
+			//loadAllClass();
+			loadteacher();
+		}
+		
+		if(type.equals("Teacher List"))
+		{
+			ListTeacherComboBox.getItems().clear();
+			for (String row : arr)
+			{
+				String[] cols = row.split(";");
+				HashMap<String, String> map = new HashMap<>();
+				for (String col : cols)
+				{
+					String[] field = col.split("=");
+					map.put(field[0], field[1]);
+				}
+
+				String username = map.get("userName");
+				String userID = map.get("userId");
+				ListTeacherComboBox.getItems().add(userID + ": " + username);
+			}
+		}
 	}
 }
