3a18bb2561358f27fcd9b6512f432c530b5ec4ce
רותם <@192.168.56.1>
2017-06-27 23:48:14 +0300

    pupil File

--- client_prototype2/src/controllers/PupilFileController.java
+++ client_prototype2/src/controllers/PupilFileController.java
@@ -1,6 +1,9 @@
 package controllers;
 
+import java.io.IOException;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.ResourceBundle;
 
 import application.Main;
@@ -11,6 +14,9 @@ import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.MenuButton;
+import javafx.scene.control.MenuItem;
+import javafx.stage.Stage;
+import ui.UserWindow;
 
 public class PupilFileController implements IController {
 
@@ -27,6 +33,12 @@ public class PupilFileController implements IController {
     private MenuButton ChooseMenuButton;
 
     @FXML
+    private MenuItem showEvaluationFormItem;
+    
+    @FXML
+    private MenuItem ShowGradeSheetIten;
+    
+    @FXML
     private ComboBox<String> CourseComboBox;
 
     @FXML
@@ -38,7 +50,45 @@ public class PupilFileController implements IController {
     @FXML
     private Label ChooseLable;
 
-    private String courseId;
+    private PupilShowEvaluationFormController PupilShowEvaluationFormController;
+	private HashMap<String, HashMap<String, String>> allCourses;
+    
+
+    void loadAllCourses()
+    {
+    	ArrayList<String> data = new ArrayList<String>();
+    	data.add("load Courses");
+    	data.add("select");
+    	data.add("courses");
+    	try
+    	{
+    		Main.client.sendToServer(data);
+    	}
+    	catch (IOException e)
+    	{
+    		e.printStackTrace();
+    	}	
+    }
+    
+	 void loadCoursesOfPupil()
+	 {
+		ArrayList<String> data = new ArrayList<String>();
+		data.add("load courses of pupil");
+		data.add("select");
+		data.add("pupil_in_course");
+	 	data.add("userID");
+	 	data.add(Main.userId);
+	
+	 	try
+	 	{
+	 		Main.client.sendToServer(data);
+	 	}
+	 	catch (IOException e)
+	 	{
+	 		e.printStackTrace();
+	 	}
+	 }
+    
     @FXML
     void ShowCourseList(ActionEvent event) {
 
@@ -46,29 +96,80 @@ public class PupilFileController implements IController {
 
     @FXML
     void BackToMenu(ActionEvent event) {
-
+        UserWindow.closeUserWindow(getClass(), (Stage)BackButton.getScene().getWindow());
     }
 
     @FXML
     void ChooseOperation(ActionEvent event) {
 
     }
+    
+    @FXML
+    void ShowEvaluationForm(ActionEvent event) {
 
+    }
+
+    @FXML
+    void ShowGradeSheet(ActionEvent event) {
+
+    }
+    
     @FXML
     void initialize() {
         assert PupilFileLabel != null : "fx:id=\"PupilFileLabel\" was not injected: check your FXML file 'PupilFile.fxml'.";
         assert ChooseMenuButton != null : "fx:id=\"ChooseMenuButton\" was not injected: check your FXML file 'PupilFile.fxml'.";
         assert CourseComboBox != null : "fx:id=\"CourseComboBox\" was not injected: check your FXML file 'PupilFile.fxml'.";
         assert BackButton != null : "fx:id=\"BackButton\" was not injected: check your FXML file 'PupilFile.fxml'.";
+        assert showEvaluationFormItem != null : "fx:id=\"showEvaluationFormItem\" was not injected: check your FXML file 'PupilFile.fxml'.";
         assert ChooseCourseLabel != null : "fx:id=\"ChooseCourseLabel\" was not injected: check your FXML file 'PupilFile.fxml'.";
+        assert ShowGradeSheetIten != null : "fx:id=\"ShowGradeSheetIten\" was not injected: check your FXML file 'PupilFile.fxml'.";
         assert ChooseLable != null : "fx:id=\"ChooseLable\" was not injected: check your FXML file 'PupilFile.fxml'.";
 
         Main.client.controller=this;
+        allCourses = new HashMap<>();
+        loadAllCourses();
     }
 
 	@Override
-	public void handleAnswer(Object msg) {
-		// TODO Auto-generated method stub
-		
+	public void handleAnswer(Object result) {
+	 	if (result == null)
+	 	{
+	 		// error
+	
+	 		return;
+	 	}
+	 	ArrayList<String> arr = (ArrayList<String>) result;
+	 	String type = arr.remove(0);
+	 	if (type.equals("load Courses"))
+	 	{
+	 		for (String row : arr)
+	 		{	
+	 			String[] cols = row.split(";");
+	 			HashMap<String, String> map = new HashMap<>();
+	 			for (String col : cols)
+	 			{
+	 				String[] field = col.split("=");
+	 				map.put(field[0], field[1]);
+	 			}
+	 			allCourses.put(map.get("courseId"), map);
+	 		}
+	 		loadCoursesOfPupil();
+	 	}
+	 	else if (type.equals("load courses of pupil"))
+	 	{
+	 		for (String row : arr)
+	 		{
+	 			String[] cols = row.split(";");
+	 			HashMap<String, String> map = new HashMap<>();
+	 			for (String col : cols)
+	 			{
+	 				String[] field = col.split("=");
+	 				map.put(field[0], field[1]);
+	 			}
+		 		
+	 			String courseId = map.get("courseID");
+	 			CourseComboBox.getItems().add(courseId + ": " + allCourses.get(courseId).get("courseName"));
+	 		}		
+	 	}
 	}
 }
