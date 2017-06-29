5606e9156f4c1cb4a1c267fdb5281c8ce9b8e0c4
DEKEL_G <DEKEL_G@DEKEL>
2017-06-29 23:20:22 +0300

    12344

--- client_prototype2/src/controllers/TeacherCheckAssignmentController.java
+++ client_prototype2/src/controllers/TeacherCheckAssignmentController.java
@@ -3,7 +3,6 @@ package controllers;
 import interfaces.IController;
 import java.net.URL;
 import java.util.ResourceBundle;
-
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
